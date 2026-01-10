7cacc91c66dd68d3ff04a31047ae471517a3b006
zainab <zainab@example.com>
2026-01-10 08:48:27 -0800

    Seed data from Excel on deploy

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py create_admin
-python manage.py seed_demo
+python manage.py seed_from_excel
