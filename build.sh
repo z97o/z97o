1edec0c1e71132bb251f6a900b77a7fc0c569807
zainab <zainab@example.com>
2026-01-10 09:12:39 -0800

    Don't fail deploy if seeding/admin fails

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py create_admin
-python manage.py seed_from_excel
 
+# لازم المايقريشن ينجح، لأنه أساس التشغيل
+python manage.py migrate
+
+# هذي لا تخلي الديبلوي يفشل لو صار خطأ
+python manage.py create_admin || true
+python manage.py seed_from_excel || true
