cff2a3098c04b45d8e1576de59bb8b22f77dd228
zainab <zainab@example.com>
2026-01-10 10:32:47 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 # لازم المايقريشن ينجح، لأنه أساس التشغيل
-python manage.py migrate
+python manage.py migrate --noinput
 
 # هذي لا تخلي الديبلوي يفشل لو صار خطأ
 python manage.py create_admin || true
